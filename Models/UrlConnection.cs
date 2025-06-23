0061484f1808e53a9df56990ae45ecd4c6a471dc
atsamd21 <atsamd21@proton.me>
2025-06-23 12:07:32 +0200

    Implement more Monero node methods

---
+++ Models/UrlConnection.cs
@@ -0,0 +1,22 @@
+﻿namespace HavenoSharp.Models;
+
+public enum OnlineStatus
+{
+    UNKNOWN = 0,
+    ONLINE = 1,
+    OFFLINE = 2
+}
+
+public enum AuthenticationStatus
+{
+    NO_AUTHENTICATION = 0,
+    AUTHENTICATED = 1,
+    NOT_AUTHENTICATED = 2
+}
+
+public class UrlConnection
+{
+    public string Url { get; set; } = string.Empty;
+    public OnlineStatus OnlineStatus { get; set; }
+    public AuthenticationStatus AuthenticationStatus { get; set; }
+}
--- Services/HavenoXmrNodeService.cs
+++ Services/HavenoXmrNodeService.cs
@@ -1,5 +1,8 @@
 ﻿using Haveno.Proto.Grpc;
 using HavenoSharp.Singletons;
+using Mapster;
+
+using static Haveno.Proto.Grpc.XmrConnections;
 using static Haveno.Proto.Grpc.XmrNode;
 
 namespace HavenoSharp.Services;
@@ -7,12 +10,17 @@ namespace HavenoSharp.Services;
 public interface IHavenoXmrNodeService
 {
     Task<bool> IsXmrNodeOnlineAsync(CancellationToken cancellationToken = default);
+    Task SetMoneroNodeAsync(string url, string username, string password, CancellationToken cancellationToken = default);
+    Task<Models.UrlConnection> GetMoneroNodeAsync(CancellationToken cancellationToken = default);
+    Task<Models.UrlConnection> CheckConnectionAsync(CancellationToken cancellationToken = default);
+    Task<List<Models.UrlConnection>> GetConnectionsAsync(CancellationToken cancellationToken = default);
 }
 
 public sealed class HavenoXmrNodeService : IHavenoXmrNodeService
 {
     private readonly GrpcChannelSingleton _grpcChannelService;
     private XmrNodeClient XmrNodeClient => new(_grpcChannelService.Channel);
+    private XmrConnectionsClient XmrConnections => new(_grpcChannelService.Channel);
 
     public HavenoXmrNodeService(GrpcChannelSingleton grpcChannelService)
     {
@@ -23,5 +31,38 @@ public sealed class HavenoXmrNodeService : IHavenoXmrNodeService
     {
         var response = await XmrNodeClient.IsXmrNodeOnlineAsync(new IsXmrNodeOnlineRequest(), cancellationToken: cancellationToken);
         return response.IsRunning;
+    }
+
+    public async Task SetMoneroNodeAsync(string url, string username, string password, CancellationToken cancellationToken = default)
+    {
+        await XmrConnections.SetConnectionAsync(new SetConnectionRequest
+        {
+            Url = url,
+            Connection = new UrlConnection
+            {
+                Priority = 1,
+                Url = url,
+                Username = username,
+                Password = password
+            }
+        }, cancellationToken: cancellationToken);
+    }
+
+    public async Task<Models.UrlConnection> GetMoneroNodeAsync(CancellationToken cancellationToken = default)
+    {
+        var response = await XmrConnections.GetConnectionAsync(new GetConnectionRequest(), cancellationToken: cancellationToken);
+        return response.Connection.Adapt<Models.UrlConnection>();
+    }
+
+    public async Task<Models.UrlConnection> CheckConnectionAsync(CancellationToken cancellationToken = default)
+    {
+        var response = await XmrConnections.CheckConnectionAsync(new CheckConnectionRequest(), cancellationToken: cancellationToken);
+        return response.Connection.Adapt<Models.UrlConnection>();
+    }
+
+    public async Task<List<Models.UrlConnection>> GetConnectionsAsync(CancellationToken cancellationToken = default)
+    {
+        var response = await XmrConnections.GetConnectionsAsync(new GetConnectionsRequest(), cancellationToken: cancellationToken);
+        return response.Connections.Adapt<List<Models.UrlConnection>>();
     }
 }
