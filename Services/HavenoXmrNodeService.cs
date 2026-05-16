f8f7e26fcc6a6608f4d5e52b95287e9892e6eb6c
atsamd21 <atsamd21@proton.me>
2026-05-16 14:32:35 +0200

    Add AddConnectionAsync method

--- Services/HavenoXmrNodeService.cs
+++ Services/HavenoXmrNodeService.cs
@@ -18,13 +18,14 @@ public interface IHavenoXmrNodeService
     Task SetAutoSwitchAsync(bool autoSwitch, CancellationToken cancellationToken = default);
     Task<bool> GetAutoSwitchAsync(CancellationToken cancellationToken = default);
     Task<Models.UrlConnection> GetBestConnectionAsync(CancellationToken cancellationToken = default);
+    Task AddConnectionAsync(string url, string username, string password, int priority, CancellationToken cancellationToken = default);
 }
 
 public sealed class HavenoXmrNodeService : IHavenoXmrNodeService
 {
     private readonly GrpcChannelSingleton _grpcChannelService;
     private XmrNodeClient XmrNodeClient => new(_grpcChannelService.Channel);
-    private XmrConnectionsClient XmrConnections => new(_grpcChannelService.Channel);
+    private XmrConnectionsClient XmrConnectionsClient => new(_grpcChannelService.Channel);
 
     public HavenoXmrNodeService(GrpcChannelSingleton grpcChannelService)
     {
@@ -39,19 +40,19 @@ public sealed class HavenoXmrNodeService : IHavenoXmrNodeService
 
     public async Task<Models.UrlConnection> GetBestConnectionAsync(CancellationToken cancellationToken = default)
     {
-        var response = await XmrConnections.GetBestConnectionAsync(new GetBestConnectionRequest(), cancellationToken: cancellationToken);
+        var response = await XmrConnectionsClient.GetBestConnectionAsync(new GetBestConnectionRequest(), cancellationToken: cancellationToken);
         return response.Connection.Adapt<Models.UrlConnection>();
     }
 
     public async Task<bool> GetAutoSwitchAsync(CancellationToken cancellationToken = default)
     {
-        var response = await XmrConnections.GetAutoSwitchAsync(new GetAutoSwitchRequest(), cancellationToken: cancellationToken);
+        var response = await XmrConnectionsClient.GetAutoSwitchAsync(new GetAutoSwitchRequest(), cancellationToken: cancellationToken);
         return response.AutoSwitch;
     }
 
     public async Task SetMoneroNodeAsync(string url, string username, string password, int priority, CancellationToken cancellationToken = default)
     {
-        await XmrConnections.SetConnectionAsync(new SetConnectionRequest
+        await XmrConnectionsClient.SetConnectionAsync(new SetConnectionRequest
         {
             Url = url,
             Connection = new UrlConnection
@@ -66,29 +67,44 @@ public sealed class HavenoXmrNodeService : IHavenoXmrNodeService
 
     public async Task<Models.UrlConnection> GetMoneroNodeAsync(CancellationToken cancellationToken = default)
     {
-        var response = await XmrConnections.GetConnectionAsync(new GetConnectionRequest(), cancellationToken: cancellationToken);
+        var response = await XmrConnectionsClient.GetConnectionAsync(new GetConnectionRequest(), cancellationToken: cancellationToken);
         return response.Connection.Adapt<Models.UrlConnection>();
     }
 
     public async Task<Models.UrlConnection> CheckConnectionAsync(CancellationToken cancellationToken = default)
     {
-        var response = await XmrConnections.CheckConnectionAsync(new CheckConnectionRequest(), cancellationToken: cancellationToken);
+        var response = await XmrConnectionsClient.CheckConnectionAsync(new CheckConnectionRequest(), cancellationToken: cancellationToken);
         return response.Connection.Adapt<Models.UrlConnection>();
     }
 
     public async Task<List<Models.UrlConnection>> GetConnectionsAsync(CancellationToken cancellationToken = default)
     {
-        var response = await XmrConnections.GetConnectionsAsync(new GetConnectionsRequest(), cancellationToken: cancellationToken);
+        var response = await XmrConnectionsClient.GetConnectionsAsync(new GetConnectionsRequest(), cancellationToken: cancellationToken);
         return response.Connections.Adapt<List<Models.UrlConnection>>();
     }
 
     public async Task RemoveConnectionAsync(string url, CancellationToken cancellationToken = default)
     {
-        await XmrConnections.RemoveConnectionAsync(new RemoveConnectionRequest() { Url = url }, cancellationToken: cancellationToken);
+        await XmrConnectionsClient.RemoveConnectionAsync(new RemoveConnectionRequest() { Url = url }, cancellationToken: cancellationToken);
     }
 
     public async Task SetAutoSwitchAsync(bool autoSwitch, CancellationToken cancellationToken = default)
     {
-        await XmrConnections.SetAutoSwitchAsync(new SetAutoSwitchRequest { AutoSwitch = autoSwitch }, cancellationToken: cancellationToken);
+        await XmrConnectionsClient.SetAutoSwitchAsync(new SetAutoSwitchRequest { AutoSwitch = autoSwitch }, cancellationToken: cancellationToken);
+    }
+
+    public async Task AddConnectionAsync(string url, string username, string password, int priority, CancellationToken cancellationToken = default)
+    {
+        await XmrConnectionsClient.AddConnectionAsync(new AddConnectionRequest
+        {
+            Connection = new UrlConnection
+            {
+                Priority = priority,
+                Url = url,
+                Username = username,
+                Password = password
+            }
+        },
+        cancellationToken: cancellationToken);
     }
 }
